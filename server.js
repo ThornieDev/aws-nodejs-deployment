0f0199a3c1c77af39c0d0e702eb60f8e60a2b05a
Kim Chanthorn <kimchanthon46@gmail.com>
2025-08-24 22:27:26 +0700

    first testing get user

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+require ('dotenv').config();
 
 
 app.get('/api/get', (req, res) => {
     res.send('Hello from Express server!'); // Respond with a simple message for the GET request
 })
+app.get('/api/get_user', (req, res) => {
+    res.send({
+        user:{
+            name: "Chanthorn",
+            age: 25,
+            contact: 1029328378,
+        }, env: process.env.NAME
+    }); // Respond with a simple message for the GET request
+})
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server is running on port 8000');
 })
